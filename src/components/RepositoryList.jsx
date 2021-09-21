7211fb80c9ffa3ae86bb7ec45ac77b04fbe65702
juliaazt <julia.beatriz@upflow.me>
2021-09-21 15:16:47 -0300

    [3] Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 const repository = {
     name:"A",
@@ -7,6 +8,21 @@ const repository = {
     link: "http://localhost:8080/"
 }
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+    
+    /*
+        Executa a função de acordo com a lista de dependencias passada no segundo parametro
+        Se a lista for vazia, executa somente uma vez
+        Se o segundo parametro não for passado, o useEffect entra em loop
+        Se uma variavel de dependencia for atualizada dentro do useEffect, também entra em loop 
+    */
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/JuliaAzt/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, []);
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
