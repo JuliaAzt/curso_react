c557f703d38ba9bccff3d6c77a119470d7dd2c74
juliaazt <julia.beatriz@upflow.me>
2021-09-21 11:11:32 -0300

    [1] Ambiente de dev e produção

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 export function App(){
-    /*throw new Error("ERRO");*/
     return <h1>Julia</h1>
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    mode:'development',
-    devtool: 'eval-source-map',
-    entry: path.resolve(__dirname,'src', 'index.jsx'),
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    },
-    resolve:{
-        extensions:[
-            '.js',
-            '.jsx'
-        ]
-    },
-    devServer: {
-        /* Monitora os arquivos estatiscos pra caso haja alguma modificação */
-        /* usar static ao invés de contentBase */
-        static: path.resolve(__dirname,'public')
-    },
-    plugins:[
-        
-        /* Gera o arquivo index.html na pasta dist, sem necessidade de fazer importação */
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname,'public', 'index.html')
-        })
-
+  mode: isDevelopment ? "development" : "production",
+  /* Para debugar código react no console */
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    /* Monitora os arquivos estatiscos pra caso haja alguma modificação */
+    /* usar static ao invés de contentBase */
+    static: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    /* Gera o arquivo index.html na pasta dist, sem necessidade de fazer importação */
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test:/\.jsx$/, 
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}
+  },
+};
