8928856b76c80eb4ae9e6ce4db125dc603bbf27b
juliaazt <julia.beatriz@upflow.me>
2021-09-21 14:08:50 -0300

    [2] Fast Refresh no WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
@@ -18,19 +19,28 @@ module.exports = {
     /* Monitora os arquivos estatiscos pra caso haja alguma modificação */
     /* usar static ao invés de contentBase */
     static: path.resolve(__dirname, "public"),
+    hot: true
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     /* Gera o arquivo index.html na pasta dist, sem necessidade de fazer importação */
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
-    }),
-  ],
+    })
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+            loader: 'babel-loader',
+            options:{
+                plugins: [
+                    isDevelopment && require.resolve('react-refresh/babel')
+                ].filter(Boolean)
+            }
+        }
       },
       {
         test: /\.scss$/,
