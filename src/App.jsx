58ca3e3ca46f89daf88f86269b907989e049bfdd
juliaazt <julia.beatriz@upflow.me>
2021-09-21 13:01:14 -0300

    [2] Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 export function App(){
-    return <RepositoryList/>
+    return (
+        /*
+        <div>
+            <RepositoryList/>
+            <Counter/>
+        </div> 
+        */
+
+        /*Fragment (<> </>) - agrupa e não é produzido no html*/
+        <> 
+            <RepositoryList/>
+            <Counter/>
+        </> 
+    )
 }
 
