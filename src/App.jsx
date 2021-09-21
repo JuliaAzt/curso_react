ea2ca70bd8bdbd9eee06b601f78720a784417a75
juliaazt <julia.beatriz@upflow.me>
2021-09-21 14:54:51 -0300

    [3] Estilização da listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Counter } from './components/Counter'
+
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 export function App(){
     return (
-        /*
-        <div>
-            <RepositoryList/>
-            <Counter/>
-        </div> 
-        */
-
-        /*Fragment (<> </>) - agrupa e não é produzido no html*/
-        <> 
-            <RepositoryList/>
-            <Counter/>
-        </> 
+        <RepositoryList/>
     )
 }
 
