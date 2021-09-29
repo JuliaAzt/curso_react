0ef9d010323c68acdae0e01300c9a49315981dbe
juliaazt <juliabeatrizazt@gmail.com>
2021-09-29 10:18:01 -0300

    [4] TypeScript no ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const isDevelopment = process.env.NODE_ENV !== "production";
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: isDevelopment ? "development" : "production",
+  mode: isDevelopment ? 'development' : 'production',
   /* Para debugar código react no console */
-  devtool: isDevelopment ? "eval-source-map" : "source-map",
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     /* Monitora os arquivos estatiscos pra caso haja alguma modificação */
     /* usar static ao invés de contentBase */
-    static: path.resolve(__dirname, "public"),
+    static: path.resolve(__dirname, 'public'),
     hot: true
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
     /* Gera o arquivo index.html na pasta dist, sem necessidade de fazer importação */
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
     })
   ].filter(Boolean),
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
         
       }
     ],
