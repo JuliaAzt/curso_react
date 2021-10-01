b6edda3355d84a9f12e4fcb578f4c891798fa62b
juliaazt <juliabeatrizazt@gmail.com>
2021-10-01 10:20:17 -0300

    [1] Estrutura com create-react-app

---
+++ Capitulo 2/dtmoney/src/App.tsx
@@ -0,0 +1,12 @@
+/* 
+  * export function -> para importar o em outro arquivo deve ser exato o nome da função
+  * export default App -> para importar em outro arquivo pode ser dado qualquer nome
+*/
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
