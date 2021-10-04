f3308311cdb2eda21f8304b14d589472ac69ed39
juliaazt <juliabeatrizazt@gmail.com>
2021-10-04 09:36:59 -0300

    [1] Instalando Styled Components

--- Capitulo 2/dtmoney/src/App.tsx
+++ Capitulo 2/dtmoney/src/App.tsx
@@ -1,12 +1,13 @@
-/* 
-  * export function -> para importar o em outro arquivo deve ser exato o nome da função
-  * export default App -> para importar em outro arquivo pode ser dado qualquer nome
-*/
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
